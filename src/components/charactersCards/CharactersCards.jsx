7f433678b5ff8d8346c89900782364a432bce2af
Fernando <fer3443@gmail.com>
2023-08-22 19:57:04 -0300

    arreglo de errores en charactersCards y navbar

--- src/components/charactersCards/CharactersCards.jsx
+++ src/components/charactersCards/CharactersCards.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-
-import { motion } from "framer-motion";
-
 import "../charactersCards/CharactersCards.css";
-
 export const CharactersCards = ({character}) => {
   
   return (
     <>
-      {character.map((item, index) => {
+      {
+			character.map((item, index) => {
         return (
-          <motion.div animate={{x:0, scale: 1 }} initial={{ x: 200, scale: 0 }} key={index}>
-            <div className="cardContainer">
-              <figure>
-                <img className="cardImage" src={item.image} alt="" />
-              </figure>
-              <div className="cardBody">
-                <h4 className="cardName">{item.name}</h4>
-                <p>Estado: {item.status}</p>
-                <p>Especie: {item.species}</p>
-                <p>Genero: {item.gender}</p>
-                <button>ver mas</button>
-              </div>
+          <div className="cardContainer" key={index}>
+            <figure>
+              <img className="cardImage" src={item.image} alt="" />
+            </figure>
+            <div className="cardBody">
+              <h4>{item.name}</h4>
+              <p>Estado: {item.status}</p>
+              <p>Genero: {item.gender}</p>
+              <p>Especie: {item.species}</p>
+              <button className="buttonYw">ver mas</button>
             </div>
-          </motion.div>
+          </div>
         );
       })}
     </>
