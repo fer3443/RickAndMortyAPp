c2652af4fdd61dd6fe12985799541e175c3579a6
Fernando <fer3443@gmail.com>
2023-08-30 01:01:59 -0300

    links agregados a socialFooter

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,18 +7,18 @@ export const Footer = () => {
 		<>
 		<div className="containerFooter">
 			<div className="footerAbout">
-				<span>El contenido de este sitio web no está asociado, ni esponsoreado o especialmente aprobado por Rick and Morty. Todo tipo de infomracion brindada en el mismo, es extraida de <Link to={"https://rickandmortyapi.com/documentation/#introduction"}>rickandmortyapi</Link> con un fin práctico y educativo en el desarrollo de apps.</span>
+				<span>El contenido de este sitio web no está asociado, ni esponsoreado o especialmente aprobado por Rick and Morty. Todo tipo de infomracion brindada en el mismo, es extraida de <Link to={"https://rickandmortyapi.com/"}>rickandmortyapi</Link> con un fin práctico y educativo en el desarrollo de apps.</span>
 			</div>
 			<div className="socials">
 				<div className="socialTitle">
 					<h6>Mis redes sociales:</h6>
 				</div>
 				<div className="linksFooter">
-				<Link className="linkSocial fb"><box-icon type='logo' color="#fff" name='facebook'animation='tada-hover'></box-icon></Link>
-				<Link className="linkSocial ig"><box-icon type='logo' color="#fff" name='instagram' animation='tada-hover'></box-icon></Link>
-				<Link className="linkSocial ds"><box-icon type='logo' color="#fff" name='discord-alt' animation='tada-hover'></box-icon></Link>
-				<Link className="linkSocial ldin"><box-icon type='logo' color="#fff" name='linkedin' animation='tada-hover'></box-icon></Link>
-				<Link className="linkSocial ghub"><box-icon type='logo' color="#fff" name='github' animation='tada-hover'></box-icon></Link>
+				<Link to={"https://www.facebook.com/fernando.arroyorodriguez.9/"} className="linkSocial fb"><box-icon type='logo' color="#fff" name='facebook'animation='tada-hover'></box-icon></Link>
+				<Link to={"https://www.instagram.com/ferchoar23/"} className="linkSocial ig"><box-icon type='logo' color="#fff" name='instagram' animation='tada-hover'></box-icon></Link>
+				<Link to={"https://discord.gg/4dFstx5X"} className="linkSocial ds"><box-icon type='logo' color="#fff" name='discord-alt' animation='tada-hover'></box-icon></Link>
+				<Link to={"https://www.linkedin.com/in/fer3443in"} className="linkSocial ldin"><box-icon type='logo' color="#fff" name='linkedin' animation='tada-hover'></box-icon></Link>
+				<Link to={"https://github.com/fer3443"} className="linkSocial ghub"><box-icon type='logo' color="#fff" name='github' animation='tada-hover'></box-icon></Link>
 				</div>
 			</div>
 		</div>
