c5ab74e6d33d2fac14527849bedd5d3b8a43c6ea
Fernando <fer3443@gmail.com>
2023-10-17 21:22:09 -0300

    logica para orden alfabetico agregado a Characters

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -13,6 +13,9 @@ import "../characters/Characters.css";
 export const Characters = () => {
   const { loading, setLoading } = useContext(LoaderProvider);
   const [character, setCharacter] = useState([]);
+  const [originalCharacter, setOriginalCharacter] = useState([]);
+  const [ isChecked, setIsChecKed] = useState(false);
+
   const pageMin = 1;
   const pageMax = 42;
   const [page, setPage] = useState(sessionStorage.getItem("page"));
@@ -24,6 +27,7 @@ export const Characters = () => {
     getAllCharactersPage(page)
       .then(({ results }) => {
         setCharacter(results);
+        setOriginalCharacter(results);
       })
       .catch((err) => console.log(err))
       .finally(() => {
@@ -32,7 +36,31 @@ export const Characters = () => {
         }, 500);
       });
   }, [page]);
+  
+  const handleChecked = () => {
+    setIsChecKed(!isChecked)
+  }
+  const restoredOrder = () => {
+    setCharacter(originalCharacter)
+  }
 
+  async function alphabeticalOrder(array){
+    try {
+      const newArray = [...array];
+      newArray.sort((a, b) => a.name.localeCompare(b.name));
+      setCharacter(newArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if(isChecked){
+      alphabeticalOrder(character)
+    }else{
+      restoredOrder()
+    }
+  }, [isChecked])
   return (
     <>
       {loading ? (
@@ -52,6 +80,10 @@ export const Characters = () => {
             pageMin={pageMin}
             pageMax={pageMax}
           />
+            <div className="inputCheck">
+              <label>Ordenar Personajes alfabeticamente</label>
+              <input type="checkbox" checked={isChecked} onChange={handleChecked} className="inputCheckBox"/>
+            </div>
           <section className="cardsContainer">
             <CharactersCards character={character} />
           </section>
