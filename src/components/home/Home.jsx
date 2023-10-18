e8420ddb572bcc452ae198b1874f0d4d52becd3f
Fernando <fer3443@gmail.com>
2023-10-18 20:28:40 -0300

    motion framer agregado a home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+
+import { motion } from "framer-motion";
+
 import { Link } from "react-router-dom";
 import { Footer } from "../footer/Footer";
 import logoHome from "../../assets/logoHome.png";
@@ -21,7 +24,14 @@ export default function Home() {
         <Loader />
       ) : (
         <div className="homeContainer">
-          <img src={logoHome} alt="" className="logoHome" />
+          <motion.div
+            initial={{scale: 0}}
+            animate={{ x: 0, scale:1  }}
+            transition={{ type: "spring", stiffness: 100}}
+          >
+            <img src={logoHome} alt="" className="logoHome" />
+          </motion.div>
+
           <section className="homeSection">
             <h1>Rick and Morty App.</h1>
             <h6>
@@ -92,103 +102,88 @@ export default function Home() {
               </div>
             </div>
           </section>
-          <section className="sectionDivisor">
-          </section>
+          <section className="sectionDivisor"></section>
           <section className="sectionTrailers">
             <header className="headerTrailers">
               <h2>Trailers de temporadas</h2>
             </header>
             <div className="trailersContainer">
               <div className="trailersBox">
-                <h6>
-                  Temporada 1
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/BFTSrbB2wII?si=2U5TmE94d5sCyXUq"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 1</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/BFTSrbB2wII?si=2U5TmE94d5sCyXUq"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
               <div className="trailersBox">
-                <h6>
-                  Temporada 2
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/_IZfO_LfK5Q?si=DIrdyepxul1GhFC-"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 2</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/_IZfO_LfK5Q?si=DIrdyepxul1GhFC-"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
               <div className="trailersBox">
-                <h6>
-                  Temporada 3
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/rLyOul8kau0?si=Vxi5ygw1SfncvX71"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 3</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/rLyOul8kau0?si=Vxi5ygw1SfncvX71"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
               <div className="trailersBox">
-                <h6>
-                  Temporada 4
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/hl1U0bxTHbY?si=1D03sntsnh1v2cov"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 4</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/hl1U0bxTHbY?si=1D03sntsnh1v2cov"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
               <div className="trailersBox">
-                <h6>
-                  Temporada 5
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/qbHYYXj2gMc?si=2P5LteNi5DQxsko4"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 5</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/qbHYYXj2gMc?si=2P5LteNi5DQxsko4"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
               <div className="trailersBox">
-                <h6>
-                  Temporada 6
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/P9WZhGMlDBE?si=p5_GOQzge-zhr9Yh"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 6</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/P9WZhGMlDBE?si=p5_GOQzge-zhr9Yh"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
               <div className="trailersBox">
-                <h6>
-                  Temporada 7
-                </h6>
-              <iframe
-                width="360"
-                height="215"
-                src="https://www.youtube.com/embed/58-gZTQ36LU?si=MASKKMXkKCMlr_v4"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
+                <h6>Temporada 7</h6>
+                <iframe
+                  width="360"
+                  height="215"
+                  src="https://www.youtube.com/embed/58-gZTQ36LU?si=MASKKMXkKCMlr_v4"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
               </div>
             </div>
           </section>
