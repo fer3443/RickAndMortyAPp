3dc4badd9f88e6724c4b83da50aa8ee53482fc5f
Fernando <fer3443@gmail.com>
2023-10-19 15:42:31 -0300

    metadatos agregados

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,25 @@ import AboutPage from "./pages/AboutPage";
 import CharacterPageDetail from "./pages/CharacterPageDetail";
 import { LoaderContext } from "./context/LoaderContext";
 
+import LocationsPageDetail from "./pages/LocationsPageDetail";
+import metaRaM from "./assets/metaRaM.jpg"
 
 import "./App.css";
-import LocationsPageDetail from "./pages/LocationsPageDetail";
+import { Helmet } from "react-helmet";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Helmet>
+      <title>Rick and Morty app</title>
+      <meta name="description" content="Informacion de la serie Rick and Morty" />
+      {/* Sitio dedicado a la serie Rick And Morty, con el consumo de api y practica en react. =)  */}
+      <meta property="og:image" content={metaRaM} />
+      <meta property="og:image:width" content="150" />
+      <meta property="og:image:height" content="170" />
+    </Helmet>
         <LoaderContext>
           <NavigationBar />
           <Routes>
